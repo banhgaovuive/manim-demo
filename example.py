6031d6c117e2be3f16749c806dd302d89f2385bf
Nguyễn Thu Giang <thugiangbanhgao@gmail.com>
2025-07-10 22:34:29 +0700

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,6 @@ from manim import *
 
 class HelloWorld(Scene):
     def construct(self):
-        text = Text("Hello, Manim on GitHub!")
+        text = Text("Hello from GitHub Actions!")
         self.play(Write(text))
         self.wait(2)
